84fd8e5f1cad8506a8d60a8cdf2e3522ae383b7b
ecardenasm <ecardenasm@unicesar.edu.co>
2024-09-04 22:19:45 -0500

    Funcion de filtrado

--- bin/actividad_users.dart
+++ bin/actividad_users.dart
@@ -7,19 +7,32 @@ void main() async {
   if (users.isEmpty) {
     print('No hay usuarios');
   } else {
-    print('hola');
+    filtroUser(users);
+    print("------------------------------------------");
+    filtroCorreo(users);
   }
 }
-  Future<List<User>> fetchUsers() async {
-    var url = Uri.parse('https://jsonplaceholder.typicode.com/users');
-    var response = await http.get(url);
-    if (response.statusCode == 200) {
-      List<User> users = (json.decode(response.body) as List)
-          .map((data) => User.fromJson(data))
-          .toList();
-      return users;
-    } else {
-      print('Error: al obtener los usuarios. ${response.statusCode}');
-      return [];
-    }
+Future<List<User>> fetchUsers() async {
+  var url = Uri.parse('https://jsonplaceholder.typicode.com/users');
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    List<User> users = (json.decode(response.body) as List)
+        .map((data) => User.fromJson(data))
+        .toList();
+    return users;
+  } else {
+    print('Error: al obtener los usuarios. ${response.statusCode}');
+    return [];
   }
+}
+
+void filtroUser(List<User> users) {
+  var user = users.where((user) => user.name.length > 6).toList();
+  user.forEach((element)=> element.imprimirUser());
+}
+
+void filtroCorreo(List<User> users) {
+  var user = users.where((user) => user.email.contains('biz')).toList();
+  print('Numero de usuarios con correo que pertenecen al dominio "biz": ${user.length}');
+  user.forEach((element)=> element.imprimirUser());
+}
